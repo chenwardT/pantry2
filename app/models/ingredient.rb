d4b90949f86f541b23cbf3fa41b06c420d805d92
Edward Chen <chenward.t@gmail.com>
2014-11-26 09:30:44 -0500

    Validating ingredient's purchase date is not in the future.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -5,6 +5,7 @@ class Ingredient < ActiveRecord::Base
   validates :user, presence: true
   validates :expiration_date, presence: true
   validates :name, presence: true
+  validate :purchase_date_cannot_be_in_the_future
 
   def expired?
     Time.now > (purchase_date + expiration_date.duration.days)
@@ -21,4 +22,12 @@ class Ingredient < ActiveRecord::Base
   def time_until_expiration_in_words
     distance_of_time_in_words(purchase_date, Time.now - expiration_date.duration.days)
   end
+
+  protected
+
+    def purchase_date_cannot_be_in_the_future
+      if purchase_date > Time.now
+        errors.add(:purchase_date, "can't be in the future")
+      end
+    end
 end
